da630b4a084a4be2e0eb8f02b0fcb3855abcbaec
nmendozam <mendozamejian@gmail.com>
2025-06-30 15:03:49 +0200

    Refactor improve code readability

--- src/alleleTools/genotype/consensus.py
+++ src/alleleTools/genotype/consensus.py
@@ -65,14 +65,18 @@ def call_function(args):
         df = pd.DataFrame(columns=VCF_ROW_REF.keys())
         position = 29910247
 
-    # Check if output file exists
-    if os.path.exists(args.out):
-        print(f"Output file {args.out} already exists.")
-        exit(1)
-
     for file in args.input:
-        report, consensus = _open_report(file)
-        alleles = [str(c) for gene in consensus for c in consensus[gene].alleles]
+        json_report = json.load(file)
+
+        report = Report(json_report)
+
+        consensus = ConsensusAlgorithm(report)
+        consensus.correct_homozygous_calls()
+        alleles = consensus.get_flat_alleles()
+
+        ##########################
+        # Generate the output file
+        ##########################
 
         if args.format == "vcf":
             # Look for existing alleles
@@ -91,16 +95,16 @@ def call_function(args):
             # Add consensus to data frame
             df = pd.concat([df, pd.DataFrame(vcf_row, index=alleles)])
         elif args.format == "alt":
-            with open(args.out, "a") as out:
+            with open(args.output, "a") as out:
                 out.write("%s\t2\t%s\n" % (report.sample, "\t".join(alleles)))
 
     if args.format == "vcf":
-        with open(args.out, "w") as vcf:
+        with open(args.output, "w") as vcf:
             vcf.write(VCF_HEADER)
 
         df.fillna("0|0:0:1,0,0", inplace=True)
         df.sort_index(inplace=True)
-        df.to_csv(args.out, sep="\t", mode="a", index=False)
+        df.to_csv(args.output, sep="\t", mode="a", index=False)
 
 
 class ComparisonResult(Enum):
@@ -112,8 +116,8 @@ class ComparisonResult(Enum):
     MORE_RESOLUTION = 4
 
 
-class AlleleParser:
-    """Pareses and compares alleles"""
+class Allele:
+    """Class to represent alleles"""
 
     def __init__(self, program_name: str, code: str) -> None:
         if not code:
@@ -137,108 +141,108 @@ class AlleleParser:
     def __repr__(self) -> str:
         if not hasattr(self, "gene"):
             return ""
+        # return f"({len(self.fields)}-fields){self.gene}*{':'.join(self.fields)}"
         return f"{self.gene}*{':'.join(self.fields)}"
 
+    def compare(self, allele: "Allele") -> ComparisonResult:
+        # Check that it belongs to the same gene
+        if self.gene != allele.gene:
+            return ComparisonResult.NOT_EQUAL
 
-class ReportParser:
-    def __init__(self, report) -> None:
+        # Compare each field
+        for s_field, a_field in zip(self.fields, allele.fields):
+            if s_field != a_field:
+                return ComparisonResult.NOT_EQUAL
+
+        # Check differences in resolution
+        if len(self.fields) > len(allele.fields):
+            return ComparisonResult.LESS_RESOLUTION
+        elif len(self.fields) < len(allele.fields):
+            return ComparisonResult.MORE_RESOLUTION
+
+        return ComparisonResult.EQUAL
+
+
+class Report:
+    def __init__(self, report: dict) -> None:
         calls = report["calls"]
         self.sample = report["sample"]
         self.genes = dict()
         for gene, call in calls.items():
             self.genes[gene] = self.parse_call(call)
 
-    def parse_call(self, call):
-        calls = list()
-        flat_list = [
-            (algorithm, called_alleles)
-            for algorithm in call
-            for called_alleles in call[algorithm]
-        ]
-        for algorithm, pred in flat_list:
-            try:
-                allele = AlleleParser(algorithm, pred)
-                calls.append(allele)
-            except:
-                pass
-        return calls
+    def parse_call(self, call: dict) -> list[Allele]:
+        alleles = list()
+
+        for algorithm, called_alleles in call.items():
+            for allele in called_alleles:
+                try:
+                    allele = Allele(algorithm, allele)
+                    alleles.append(allele)
+                except Exception as e:
+                    print(f"Error parsing allele {allele} from {algorithm} algorithm")
+                    print("\t%s" % e)
+                    pass
+        return alleles
 
 
 class ConsensusAlgorithm:
-    def __init__(self, gene_name: str) -> None:
-        self.gene_name = gene_name
-        self.alleles = list()
+    def __init__(self, report: Report) -> None:
+        self.consensus = dict()
 
-    def __repr__(self) -> str:
-        return f"{self.gene_name}->{self.alleles}"
+        for gene, calls in report.genes.items():
+            alleles = list()
+
+            # Cluster similar alleles
+            for call in calls:
+                match = self.find_matching_allele(call, alleles)
+
+                if not match:
+                    alleles.append(self.Consensus(call))
+                else:
+                    match.add_evidence(call)
+
+                    if match.compare(call) == ComparisonResult.MORE_RESOLUTION:
+                        match.fields = call.fields
+
+            self.consensus[gene] = alleles
+
+    def correct_homozygous_calls(self):
+        """Go over alleles and correct homozygosity"""
+        for gene in self.consensus:
+            n_alleles = len(self.consensus[gene])
+
+            if n_alleles > 2:
+                self.consensus[gene] = self.consensus[gene][:2]
+            elif n_alleles == 1:
+                self.consensus[gene] = self.consensus[gene] * 2
+            elif n_alleles == 0:
+                self.consensus[gene] = ["NA", "NA"]
+
+    def find_matching_allele(self, allele: Allele, allele_list):
+        """Finds a matching allele in the consensus"""
+        for a in allele_list:
+            result = a.compare(allele)
+            if result != ComparisonResult.NOT_EQUAL:
+                return a
+        return None
+
+    def get_flat_alleles(self) -> list[str]:
+        return [str(c) for gene in self.consensus for c in self.consensus[gene]]
 
     class Consensus:
         def __init__(self, new) -> None:
-            self.gene = new.gene
-            self.fields = new.fields
+            self.main_allele: Allele = new
             self.evidence = [new]
 
         def __repr__(self) -> str:
-            return f"{self.gene}*{':'.join(self.fields)}"
-            # return f"{self.gene}*{':'.join(self.fields)} ({len(self.evidence)})"
-
-        def is_equal(self, allele: AlleleParser):
-            # Check that it belongs to the same gene
-            if self.gene != allele.gene:
-                return ComparisonResult.NOT_EQUAL
-
-            # Compare each field
-            for s_field, a_field in zip(self.fields, allele.fields):
-                if s_field != a_field:
-                    return ComparisonResult.NOT_EQUAL
-
-            # Check differences in resolution
-            if len(self.fields) > len(allele.fields):
-                return ComparisonResult.LESS_RESOLUTION
-            elif len(self.fields) < len(allele.fields):
-                return ComparisonResult.MORE_RESOLUTION
-
-            return ComparisonResult.EQUAL
-
-    def add(self, new_pred):
-        no_matches = True
-        for pred in self.alleles:
-            result = pred.is_equal(new_pred)
-            if result == ComparisonResult.NOT_EQUAL:
-                continue
-
-            pred.evidence.append(new_pred)
-            no_matches = False
-
-            if result == ComparisonResult.MORE_RESOLUTION:
-                pred.fields = new_pred.fields
-
-        if no_matches:
-            self.alleles.append(self.Consensus(new_pred))
+            return str(self.main_allele)
+            # return f"{self.main_allele} ({len(self.evidence)})"
+        
+        def add_evidence(self, allele: Allele) -> None:
+            self.evidence.append(allele)
+        
+        def compare(self, allele: Allele) -> ComparisonResult:
+            return self.main_allele.compare(allele)
 
 
-def _open_report(file_name: str):
-    with open(file_name) as f:
-        json_report = json.load(f)
-
-        print(file_name)
-        report = ReportParser(json_report)
-        consensus = dict()
-        for gene, calls in report.genes.items():
-            consensus[gene] = ConsensusAlgorithm(gene)
-            for call in calls:
-                consensus[gene].add(call)
-
-            # Print consensus that have more than one allele
-            if len(consensus[gene].alleles) > 2:
-                for call in calls:
-                    print("\t", call, call.program_name)
-                print("consensus:", consensus[gene])
-                consensus[gene].alleles = consensus[gene].alleles[:2]
-            elif len(consensus[gene].alleles) == 1:
-                consensus[gene].alleles = consensus[gene].alleles * 2
-            elif len(consensus[gene].alleles) == 0:
-                consensus[gene].alleles = ["NA", "NA"]
-
-            assert len(consensus[gene].alleles) == 2
-        return report, consensus
