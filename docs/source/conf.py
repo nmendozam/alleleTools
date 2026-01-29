b8d8f95beb565c5663cc2cba2ebc68f877afbb2e
nmendozam <mendozamejian@gmail.com>
2026-01-29 16:00:20 +0100

    Add api docs to read the docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,5 +1,10 @@
 # Configuration file for the Sphinx documentation builder.
-#
+
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../../src")))
+
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
@@ -20,6 +25,9 @@ extensions = [
     "sphinx.ext.viewcode",
 ]
 
+# Optional runtime dependencies:
+autodoc_mock_imports = ["swifter"]
+
 templates_path = ['_templates']
 exclude_patterns = []
 
@@ -32,3 +40,12 @@ html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
 html_favicon = '_static/logo.svg'
+
+# GitHub integration - makes "View page source" link to GitHub
+html_context = {
+    "display_github": True,
+    "github_user": "nmendozam",
+    "github_repo": "alleleTools",
+    "github_version": "main",
+    "conf_py_path": "/docs/source/",
+}
